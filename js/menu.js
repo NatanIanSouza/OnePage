e0627593a1784b75fcdc477ee2ac952de1fe42a2
NatanIanSouza <iannatan4@gmail.com>
2021-09-13 12:28:12 -0300

    commit - tentativa e erro

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,39 @@ $(document).ready(function(){
         e.preventDefault()
         $('#menu').toggle('slow')
     })
+
+    //Criar uma função para monitorar os links
+    $('a').click(function(e){
+    e.preventDefault()
+    
+    let url = $(this).attr('href');
+
+    if(url == 'contact'){
+        $('#'+url).show();
+        $('#home').hide();
+        $('#action').hide();
+        $('#about').hide();
+    }
+
+    else if (url == 'home'){
+        $('#'+url).show();
+        $('#contact').hide();
+        $('#action').hide();
+        $('#about').hide();
+    }
+    
+    else if (url == 'action'){
+        $('#'+url).show();
+        $('#contact').hide();
+        $('#home').hide();
+        $('#about').hide();
+    }
+
+    else if (url == 'about'){
+        $('#'+url).show();
+        $('#contact').hide();
+        $('#action').hide();
+        $('#home').hide();
+    }
+    })
 })
